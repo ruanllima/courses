bcef008106c5105512b31759f71fa6e3532a9e8f
ruanllima <ruanlimafn@gmail.com>
2025-03-18 21:09:06 -0300

    PHP | PHPPRO | Dinamics routes params

--- php/phppro/app/router/router.php
+++ php/phppro/app/router/router.php
@@ -15,26 +15,48 @@ function exactMatchUriInArrayRoutes($uri, $routes)
     return [];
 }
 
+function regexMatchArrayRoutes($uri, $routes)
+{
+    return array_filter(
+        $routes,
+        function($value) use($uri){
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+function params($uri, $matchedUri)
+{
+    if(!empty($matchedUri))
+    {
+        $matchedToGetParams = array_keys($matchedUri)[0];
+            return array_diff(
+                explode('/', ltrim($uri, '/')),
+                explode('/', ltrim($matchedToGetParams, '/'))
+            );
+    }
+    return [];
+}
+
 function router()
 {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
 
+
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
 
     if(empty($matchedUri))
     {
-        $matchedUri = array_filter(
-            $routes,
-            function($value) use($uri){
-                $regex = str_replace('/', '\/', ltrim($value, '/'));
-                return preg_match("/^$regex$/", ltrim($uri, '/'));
-            },
-            ARRAY_FILTER_USE_KEY
-        );
+        $matchedUri = regexMatchArrayRoutes($uri, $routes);
+        $params = params($uri, $matchedUri);
+        var_dump($params);
+        die();
     }
 
-    
+
 
     var_dump($matchedUri);
     die();
--- php/phppro/app/router/routes.php
+++ php/phppro/app/router/routes.php
@@ -3,5 +3,7 @@
 return [
     '/' => 'Home@index',
     '/user/create' => 'User@create',
-    '/user/[a-z0-9]+' => 'User@index'
+    '/user/[0-9]+' => 'User@index',
+    '/user/[0-9]+/name/[a-z]+' => 'User@show'
+
 ];
